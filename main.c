0ee2c3ade84542778d7f52551b1a2de929686ba4
lyj25-stack <lyj25@sookmyung.ac.kr>
2025-10-16 15:52:11 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,22 +3,28 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int inc(int counter);
+
+
+
+
+void f(void);
 int main(int argc, char *argv[])
 {
-int i=10;
-printf("함수 호출 전 i=%d\n",i);
-i=inc(i);
-printf("함수 호출 후 i=%d\n",i);
-
+int i;
+for (i=0;i<5;i++)
+{
+f();
+}
 system("PAUSE");
 return 0;
 }
-int inc(int counter)
+void f(void)
 {
-counter++;
-return counter;
+int i;
+for (i=0;i<10;i++)
+printf("#");
 }
+
  
 
 
