a9fdcef0b1d5cbbd55e213f710f02bb32ad9c0a3
lyj25-stack <lyj25@sookmyung.ac.kr>
2025-10-16 15:26:59 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,15 +5,15 @@
 
 
 int main(int argc, char *argv[])
-{int flag=1;
-int y;
-
-while(flag!=0)
 {
-y=3;
-flag=0;
+int i;
+
+for (i=0;i<5;i++)
+{
+static int temp=1;
+printf("temp=%d\n",temp);
+temp++;
 }
-y=4;
 
 
 system("PAUSE");
